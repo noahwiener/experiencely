d7083ba3e7d7c25315b08b5740577dfd27efaab8
noahwiener <noah.wiener@gmail.com>
2015-10-14 17:36:32 -0700

    move image to public

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -10,7 +10,7 @@ Navbar = React.createClass({
     return (
       <div id="navbar" className="navbar-collapse collapse noah-nav">
         <ul className="nav navbar-nav">
-          <li className="active"><a href="/#browse"><img src="assets/logo.png" alt="logo" height="60px" /></a></li>
+          <li className="active"><a href="/#browse"><img src="images/logo.png" alt="logo" height="60px" /></a></li>
         </ul>
         <ul className="nav navbar-nav navbar-right">
           {x}
