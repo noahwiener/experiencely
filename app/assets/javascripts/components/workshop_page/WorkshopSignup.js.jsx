3928521ed7c9819629995f2c8ac7d0a337a8414f
noahwiener <noah.wiener@gmail.com>
2015-10-19 18:53:45 -0700

    add safety for logged out users

--- app/assets/javascripts/components/workshop_page/WorkshopSignup.js.jsx
+++ app/assets/javascripts/components/workshop_page/WorkshopSignup.js.jsx
@@ -4,6 +4,13 @@ WorkshopSignup = React.createClass({
   componentWillReceiveProps: function (props) {
   },
 
+  _signup: function(){
+    if (window.CURRENT_USER_UNAME){
+      ApiUtil.signUp(this.props.workshop.id);
+    } else {
+      alert("please sign in to sign up for an event");
+    }
+  },
   render: function(){
     if (this.props.workshop.isSignedUp){
       return (<div>
@@ -11,7 +18,7 @@ WorkshopSignup = React.createClass({
                 <div onClick={ApiUtil.cancelSignUp.bind(null, this.props.workshop.id)} className="btn signup-btn">Cancel Reservation</div>
               </div>);
     }else {
-      return(<div onClick={ApiUtil.signUp.bind(null, this.props.workshop.id)} className="btn signup-btn">SIGN UP FOR THIS WORKSHOP</div>);
+      return(<div onClick={this._signup} className="btn signup-btn">SIGN UP FOR THIS WORKSHOP</div>);
     }
 
 }
